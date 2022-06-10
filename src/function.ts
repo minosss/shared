3cea3d6903fc544db440816856cd10bfc12746db
minosss <minoscc+dev@gmail.com>
2022-06-10 21:11:47 +0800

    feat: add `runIfFn`

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,11 @@
+import {isFunction} from './is';
+
 export const noop = () => {};
 
 export function sleep(time = 3000) {
 	return new Promise<void>((r) => setTimeout(r, time));
+}
+
+export function runIfFn<T, U>(valueOrFn: T | ((...args: U[]) => T), ...args: U[]): T {
+	return isFunction(valueOrFn) ? valueOrFn(...args) : valueOrFn;
 }
