de89876dd22ee4003588331e7b07461715648034
minosss <minoscc+dev@gmail.com>
2022-07-04 23:54:32 +0800

    feat(function): add pipe function

--- src/function.ts
+++ src/function.ts
@@ -9,3 +9,12 @@ export function sleep(time = 3000) {
 export function runIfFn<T, U>(valueOrFn: T | ((...args: U[]) => T), ...args: U[]): T {
 	return isFunction(valueOrFn) ? valueOrFn(...args) : valueOrFn;
 }
+
+export function pipe<R>(...fns: Array<(a: R) => R>) {
+	return (r: R) => {
+		for (const fn of fns) {
+			r = fn(r);
+		}
+		return r;
+	};
+}
