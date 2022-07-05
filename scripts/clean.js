a72ee488b6c708e76f126961d6b17e3a563aac59
minosss <minoscc+dev@gmail.com>
2022-07-05 23:09:48 +0800

    fix: fix remove not found dir, ensure it exists

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,8 +1,11 @@
-import {rmSync} from 'node:fs';
+import {rmSync, existsSync} from 'node:fs';
 import {join} from 'node:path';
 
 const packages = ['shared', 'react-shared'];
 
 for (const name of packages) {
-	rmSync(join(process.cwd(), 'packages', name, 'dist'), {recursive: true});
+	const dist = join(process.cwd(), 'packages', name, 'dist');
+	if (existsSync(dist)) {
+		rmSync(dist, {recursive: true});
+	}
 }
