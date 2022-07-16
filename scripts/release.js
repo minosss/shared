6fdb2004d5bf3019f1dc50a40b14791a16a41083
minosss <minoscc+dev@gmail.com>
2022-07-16 23:01:23 +0800

    chore: release by semver

--- scripts/release.js
+++ scripts/release.js
@@ -1,6 +1,7 @@
 import {readFile, writeFile} from 'node:fs/promises';
 import {join} from 'node:path';
 import {execSync} from 'node:child_process';
+import semver from 'semver';
 
 function isClean() {
 	const r = execSync('git status -s', {encoding: 'utf8'});
@@ -17,6 +18,8 @@ async function writeJson(path, data) {
 }
 
 async function run() {
+	const [release = 'patch'] = process.argv.slice(2);
+
 	if (!isClean()) {
 		throw new Error(`git workspace is not clean, commit the changes or make stash`);
 	}
@@ -24,19 +27,28 @@ async function run() {
 	const pkg = await readJson('package.json');
 	const {version} = pkg;
 
+	const nextVersion = semver.inc(version, release);
+
+	if (!nextVersion) {
+		throw new Error(`version ${nextVersion} is invalid`);
+	}
+
 	const packages = ['shared', 'react-shared'];
 	for (const name of packages) {
 		const pkgPath = join(process.cwd(), 'packages', name, 'package.json');
 		const ipkg = await readJson(pkgPath);
-		ipkg.version = version;
+		ipkg.version = nextVersion;
 
 		await writeJson(pkgPath, ipkg);
-		console.log(`Updated version @yme/${name}@${version}`);
+		console.log(`Updated version @yme/${name}@${nextVersion}`);
 	}
 
+	pkg.version = nextVersion;
+	await writeJson('package.json', pkg);
+
 	execSync('git add .', {stdio: 'inherit'});
-	execSync(`git commit -m "chore(release): prepare for v${version}"`, {stdio: 'inherit'});
-	execSync(`git tag -a v${version} -m "v${version}"`, {stdio: 'inherit'});
+	execSync(`git commit -m "chore(release): prepare for v${nextVersion}"`, {stdio: 'inherit'});
+	execSync(`git tag -a v${nextVersion} -m "v${nextVersion}"`, {stdio: 'inherit'});
 }
 
 await run();
