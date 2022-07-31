71c7bfec3b17fef5b544d1e52e41241e3a3fc1e9
minosss <minoscc+dev@gmail.com>
2022-07-31 17:22:33 +0800

    chore: `mergeRefs` can pass `null` value

--- packages/react-shared/src/utils/refs.ts
+++ packages/react-shared/src/utils/refs.ts
@@ -1,10 +1,12 @@
-import {isFunction} from '@yme/shared';
+import {isFunction, isNull} from '@yme/shared';
 
-export type ReactRef<T> = React.RefCallback<T> | React.MutableRefObject<T | null>;
+export type ReactRef<T> = null | React.RefCallback<T> | React.MutableRefObject<T | null>;
 
 export function mergeRefs<T = any>(...refs: ReactRef<T>[]): React.RefCallback<T> {
 	return (value) => {
 		for (const ref of refs) {
+			if (isNull(ref)) continue;
+
 			if (isFunction(ref)) {
 				ref(value);
 				continue;
