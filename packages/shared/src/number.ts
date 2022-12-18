6faf195d9027bb3a1b9093b2fef655429759a7c7
minosss <minoscc+dev@gmail.com>
2022-12-18 13:06:15 +0800

    feat(number): adds minFn, maxFn, clampFn

--- packages/shared/src/number.ts
+++ packages/shared/src/number.ts
@@ -4,3 +4,17 @@
 export function clamp(value: number, min = 0, max = 1): number {
 	return Math.min(Math.max(value, min), max);
 }
+
+export const minFn = (min: number) => (value: number) =>
+	Number.isNaN(value) ? min : Math.max(min, value);
+
+export const maxFn = (max: number) => (value: number) =>
+	Number.isNaN(value) ? max : Math.min(max, value);
+
+export const clampFn = (min: number, max: number) => {
+	const _min = minFn(min);
+	const _max = maxFn(max);
+	return (value: number) => {
+		return _max(_min(value));
+	};
+};
